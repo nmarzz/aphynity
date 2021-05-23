0420c9b9f1de8269eac4a9b59133d2e662c7555f
nmarzz <ndmarshall97@gmail.com>
2021-05-23 18:56:56 -0400

    need to work on model

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from models.models import PendulumModel
 
 
 def loss_fn(model,train):
-    train = train.to(device)
 
+    train = train.to(device)
     output = odeint_adjoint(model, train[:,0,:], t, method='dopri5').transpose(0,1)
     l2norm = torch.linalg.norm(output - train,dim = 2)
     l2loss = torch.sum(l2norm)
@@ -25,29 +25,28 @@ t = torch.linspace(t0, te, 40)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-
-model = PendulumModel(frictionless = True,include_data = True).to(device)
 train,val,test = data_utils.get_pendulum_datasets()
-loss = loss_fn(model,train)
+model = PendulumModel(frictionless = True,include_data = True).to(device)
+output = odeint_adjoint(model, train[:,0,:], t, method='dopri5').transpose(0,1)
 
 
-
-
-
-optimizer = optim.SGD(model.parameters(), lr=0.1, momentum = 0.9)
-
-print('Training...')
-for i in range(5):
-    print(f'Epoch {i}')
-    optimizer.zero_grad()
-    loss = loss_fn(model,train)
-    loss.backward()
-    optimizer.step()
-
-
-# output = odeint_adjoint(model, y0, t, method='dopri5').transpose(0,1)
-#
-#
 # plt.plot(t,output[0,:,0].detach())
 # plt.plot(t,test[0,:,0])
 # plt.show()
+
+
+
+
+
+
+
+
+# optimizer = optim.SGD(model.parameters(), lr=0.1, momentum = 0.9)
+#
+# print('Training...')
+# for i in range(5):
+#     print(f'Epoch {i}')
+#     optimizer.zero_grad()
+#     loss = loss_fn(model,train)
+#     loss.backward()
+#     optimizer.step()
