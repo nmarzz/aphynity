8b5f0fac33ec0d219079ba932e54384264f59b44
nmarzz <ndmarshall97@gmail.com>
2021-05-24 18:33:05 -0400

    not quite converging

--- main.py
+++ main.py
@@ -21,37 +21,41 @@ init_state = train[:,0,:]
 
 
 # Define accessories
-optimizer = optim.Adam(model.parameters(), lr=0.002)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 loss_function = torch.nn.MSELoss(reduction = 'mean')
 
 
 # Training loop
 # TODO: Add testing loop
 lam = 1
-for i in range(50):
+for i in range(70):
     optimizer.zero_grad()
-    sol = odeint_adjoint(model,init_state , t, atol=1e-2, rtol=1e-2,method='dopri5')
-    pos = sol[:,:,0].transpose(0,1)
-    vel = sol[:,:,1].transpose(0,1)
-    data_loss = torch.sum(torch.linalg.norm(model.data_driven(train),dim=2))/batch_size
-    l2_loss = lam * (loss_function(pos,train[:,:,0]) + loss_function(vel,train[:,:,1]))
+    sol = odeint_adjoint(model,init_state , t, atol=1e-2, rtol=1e-2,method='dopri5').transpose(0,1)
+    # data_loss = torch.sum(torch.linalg.norm(model.data_driven(train),dim=2)**2)
+    data_loss = 0
+    l2_loss = torch.sum(torch.linalg.norm(sol - train,dim = 2)) / batch_size
     loss = data_loss + l2_loss
-    print('Backpropping')
     loss.backward()
     optimizer.step()
 
-    # Track physical model parameters
-    print('*' * 20)
-    print(f'Data contribution = {data_loss}')
-    print(f'loss contribution =  {l2_loss}')
-    print(loss)
-    for name, param in model.named_parameters():
-        if param.requires_grad and name in ['omega','alpha']:
-            print(name, param.data)
+
+
+    if i % 1 == 0:
+        # Track physical model parameters
+        print('*' * 20)
+        print(f'iteration {i}')
+        print(f'Data contribution = {data_loss}')
+        print(f'loss contribution =  {l2_loss}')
+        print(loss)
+        for name, param in model.named_parameters():
+            if param.requires_grad and name in ['omega','alpha']:
+                print(name, param.data)
+
+
 
 sol = odeint_adjoint(model,init_state , t, atol=1e-4, rtol=1e-4,method='dopri5')
 pos = sol[:,:,0].transpose(0,1)
 vel = sol[:,:,1].transpose(0,1)
-plt.plot(pos[0,:].detach())
-plt.plot(train[0,:,0])
+plt.plot(t,pos[0,:].detach())
+plt.plot(t,train[0,:,0])
 plt.show()
