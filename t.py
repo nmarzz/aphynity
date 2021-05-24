f7b929a25c52dee58c933582cb3e6594c9c56434
nmarzz <ndmarshall97@gmail.com>
2021-05-24 15:53:30 -0400

    reduce tolerance

--- t.py
+++ t.py
@@ -12,14 +12,14 @@ print(f'Training on {device}')
 t0 = 0.
 te = 20.
 t = torch.linspace(t0, te, 40).to(device)
-model = PendulumModel(frictionless = False,include_data = True).to(device)
+model = PendulumModel(frictionless = True,include_data = True).to(device)
 train,val,test = data_utils.get_pendulum_datasets(n=25)
 train,val,test = train.to(device),val.to(device),test.to(device)
 init_state = train[:,0,:]
 
 
 # Define accessories
-optimizer = optim.Adam(model.parameters(), lr=0.03)
+optimizer = optim.Adam(model.parameters(), lr=0.05)
 loss_function = torch.nn.MSELoss(reduction = 'sum')
 
 
@@ -27,14 +27,12 @@ loss_function = torch.nn.MSELoss(reduction = 'sum')
 # TODO: Add testing loop
 for i in range(50):
     optimizer.zero_grad()
-    print('Integrating')
-    sol = odeint_adjoint(model,init_state , t, atol=1e-8, rtol=1e-8,method='dopri5')
+    sol = odeint_adjoint(model,init_state , t, atol=1e-4, rtol=1e-4,method='dopri5')
     pos = sol[:,:,0].transpose(0,1)
     vel = sol[:,:,1].transpose(0,1)
     loss = loss_function(pos,train[:,:,0]) + loss_function(vel,train[:,:,1])
     print('Backpropping')
     loss.backward()
-    print('Stepping')
     optimizer.step()
 
     # Track physical model parameters
@@ -44,8 +42,11 @@ for i in range(50):
         if param.requires_grad and name in ['omega','alpha']:
             print(name, param.data)
 
-# Plot the end results
-pos,vel = odeint_adjoint(model,init_state , t, atol=1e-8, rtol=1e-8,method='dopri5')
-plt.plot(t,pos[:,0].detach())
-plt.plot(t,train[0,:,0])
+
+
+sol = odeint_adjoint(model,init_state , t, atol=1e-4, rtol=1e-4,method='dopri5')
+pos = sol[:,:,0].transpose(0,1)
+vel = sol[:,:,1].transpose(0,1)
+plt.plot(pos[0,:].detach())
+plt.plot(train[0,:,0])
 plt.show()
