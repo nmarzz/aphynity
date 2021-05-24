95a5994d1f3a79b54e3aa36f1048796199ef46a4
nmarzz <ndmarshall97@gmail.com>
2021-05-24 11:40:44 -0400

    add new models

--- models/models.py
+++ models/models.py
@@ -4,46 +4,52 @@ from torchdiffeq import odeint_adjoint
 import matplotlib.pyplot as plt
 
 
-class ODEfunc(nn.Module):
-    '''
-    Describes the ODE equation of a frictionless pendulum
-    '''
-    def __init__(self,frictionless):
-        super(ODEfunc, self).__init__()
-
-        self.omega = nn.Parameter(torch.rand(1))
-
-        if frictionless:
-            self.alpha = 0
-        else:
-            self.alpha = nn.Parameter(torch.rand([1]))
-
-    def forward(self,t,x):
-        pos , vel = x[0][0],x[0][1]
-        dpos = vel
-        dvel = (-self.omega ** 2) * torch.sin(pos) - self.alpha * vel
-
-
-        return torch.tensor([[dpos,dvel]])
-
 
 class PendulumModel(nn.Module):
     def __init__(self,frictionless = True,include_data = True):
         super(PendulumModel, self).__init__()
 
         self.include_data = include_data
+        self.frictionless = frictionless
+        if frictionless:
+            self.alpha = 0
+        else:
+            self.alpha = nn.Parameter(torch.rand([1]))
+        self.omega = nn.Parameter(torch.rand([1]))
 
-        self.data_driven = nn.Sequential(nn.Linear(2,200),
-        nn.ReLU(),
-        nn.Linear(200,200),
-        nn.ReLU(),
-        nn.Linear(200,2)
-        )
+        self.updatemat = torch.tensor([[0.,1.],[0.,- self.alpha]]).t()
 
-        self.physical = ODEfunc(frictionless)
+        if self.include_data:
+            self.data_driven = nn.Sequential(nn.Linear(2,200),
+            nn.ReLU(),
+            nn.Linear(200,200),
+            nn.ReLU(),
+            nn.Linear(200,2)
+            )
 
     def forward(self,t,x):
-        if self.include_data:            
-            return self.physical(t,x) + self.data_driven(x)
+
+        physical = torch.matmul(x,self.updatemat)
+        physical[:,1] += - (self.omega ** 2) * torch.sin(x[:,0])
+
+        if self.include_data:
+            return physical + self.data_driven(x)
         else:
-            return self.physical(t,x)
+            return physical
+
+
+
+
+
+
+class PendulumModel2(nn.Module):
+    def __init__(self):
+        super(PendulumModel2, self).__init__()
+        self.omega = nn.Parameter(torch.rand([1]))
+
+    def forward(self,t,x):        
+        pos,vel = x
+        dpos = vel
+        dvel = -self.omega**2 * torch.sin(pos)
+
+        return dpos,dvel
---
+++ t2.py
@@ -0,0 +1,56 @@
+import torch
+from torchdiffeq import odeint_adjoint
+import matplotlib.pyplot as plt
+import torch.optim as optim
+from utils import data_utils
+from models.models import PendulumModel2
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = PendulumModel2().to(device)
+train,val,test = data_utils.get_pendulum_datasets(n=1)
+t0 = 0.
+te = 20.
+t = torch.linspace(t0, te, 40)
+
+def loss_fn(model,train):
+    train = train.to(device)
+    output = odeint_adjoint(model, (0,1), t, atol=1e-8, rtol=1e-8, method='dopri5').transpose(0,1)
+    l2norm = torch.linalg.norm(output - train,dim = 2)
+    l2loss = torch.sum(l2norm)
+
+    return l2loss
+
+# for name, param in model.named_parameters():
+#     if param.requires_grad:
+#         print(name, param.data)
+
+#
+# print('Training...')
+# optimizer = optim.SGD(model.parameters(), lr=0.1, momentum = 0.9)
+# for i in range(2):
+#     optimizer.zero_grad()
+#     loss = loss_fn(model,train)
+#     print('Computing gradient')
+#     loss.backward()
+#     optimizer.step()
+
+
+init_state = (train[0,0,0] ,train[0,0,1])
+print(init_state)
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum = 0.9)
+
+for i in range(5):
+    optimizer.zero_grad()
+    pos,vel = odeint_adjoint(model,init_state , t, atol=1e-8, rtol=1e-8,method='dopri5')
+    pos_loss = pos - train[0,:,0]
+    vel_loss = vel - train[0,:,1]
+    loss = torch.sum(torch.sqrt(pos_loss ** 2 + vel_loss**2))
+    loss.backward()
+    optimizer.step()
+
+
+
+# plt.plot(t,pos.detach())
+# plt.plot(t,train[0,:,0])
+# plt.show()
